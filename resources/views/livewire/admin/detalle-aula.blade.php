dd97fed751484b936a0e50118599f537844693ca
Piero <varaspiero19@gmail.com>
2025-04-23 10:45:26 -0500

    add: detalle - aula vista

--- resources/views/livewire/admin/detalle-aula.blade.php
+++ resources/views/livewire/admin/detalle-aula.blade.php
@@ -1,4 +1,24 @@
-<div>
-    Detalle del Aula con ID: {{ $aulaId }}
-    <h1>ACCEDIO DETALLE AULA</h1>
+<div class="custom-px">
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Aulas') }}
+        </h2>
+    </x-slot>
+
+    <div class="bg-white rounded-lg shadow p-6 mt-6">
+        <h3 class="text-lg font-bold mb-2">Datos del Aula</h3>
+        <p>Información detallada del aula aquí.</p>
+    </div>
+
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 mt-6">
+        <div class="bg-white rounded-lg shadow p-6">
+            <h3 class="text-lg font-bold mb-2">ALUMNOS</h3>
+            <p>Contenido para la primera mitad.</p>
+        </div>
+
+        <div class="bg-white rounded-lg shadow p-6">
+            <h3 class="text-lg font-bold mb-2">SESIONES</h3>
+            <p>Contenido para la segunda mitad.</p>
+        </div>
+    </div>
 </div>
